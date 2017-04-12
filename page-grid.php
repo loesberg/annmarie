54026b7b461d08194b3a4cdccec12d574c27b871
loesberg <jesseloesberg@gmail.com>
2017-04-12 16:27:09 -0700

    Add page-grid.php template

---
+++ page-grid.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+* Template Name: Page with Grid
+*/
+
+get_header(); ?>
+
+		
+	<div id="content">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+</header>
+<?php amd_get_custom_sidebar(get_the_ID()); ?>
+<section class="entry-content">
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
+<?php endwhile; endif; ?>
+</section>
+</article>
+
+<div id="amd-widget-grid">
+	<?php dynamic_sidebar(' in-page-widget-area' ); ?>
+</div>
+
+	</div><!-- close content-->
+	<div style="border-top: 1px dotted #7D460B; width: 200px; margin: 0 auto; height: 20px;"></div>
+
+	<?php amd_get_footer_quote(get_the_ID()); ?>
+	</div><!-- close main-box -->
+<?php get_footer(); ?>
+	
